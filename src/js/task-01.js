f461d0098fbb0920a558c053ca1380b53a35b4ae
Vlad Pazyna <vladpazyna@gmail.com>
2022-10-23 23:15:21 +0300

    2 done

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,9 @@
 const categories = document.querySelectorAll('.item');
-console.log('Number of categories: ', categories.length);
+console.log(`Number of categories: ${categories.length} \n\n`);
 
 for (const category of categories) {
     console.log(`Category: ${category.firstElementChild.textContent}`);
-    console.log(`Elements: ${category.lastElementChild.children.length}`);
+    console.log(`Elements: ${category.lastElementChild.children.length} \n\n`);
 }
 
 
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients')
+console.log(ingredientsEl);
+
+const liElArr = [];
+
+for (const ingredient of ingredients) {
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+  liEl.classList.add('item');
+  liElArr.push(liEl);
+}
+
+ingredientsEl.append(...liElArr);
