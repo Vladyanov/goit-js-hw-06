e00d8cbe80db091a2d863d0326099753fc26b13c
Vlad Pazyna <vladpazyna@gmail.com>
2022-10-26 23:03:09 +0300

    Update task-07.js

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,11 +1,7 @@
 const inputEl = document.querySelector('#font-size-control');
 const scaleTextEl = document.querySelector('#text');
 
-console.log(inputEl);
-console.log(scaleTextEl.style.fontSize);
-
 const onSliderChange = (event) => {
-    console.log(event.currentTarget.value);
     scaleTextEl.style.fontSize = `${event.currentTarget.value}px`;
 }
 
